553a291f55d8bd9f49c52808eb5fe3d45f9238c3
sguzunov <sguzunov@gmail.com>
2016-10-30 23:49:19 +0200

    Implemented generic repository for accessing SQL Db.

---
+++ NBAStatistics.Repositories/Contracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NBAStatistics.Repositories.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> Find(Func<TEntity, bool> predicate);
+
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ NBAStatistics.Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace NBAStatistics.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using Contracts;
+
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly DbContext Context;
+
+        public GenericRepository(DbContext context)
+        {
+            this.Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            this.Context.Set<TEntity>().Add(entity);
+            this.Context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+
+        }
+
+        public IEnumerable<TEntity> Find(Func<TEntity, bool> predicate)
+        {
+            var result = this.Context.Set<TEntity>()
+                .Where(predicate)
+                .ToList();
+            return result;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            var result = this.Context.Set<TEntity>().ToList();
+            return result;
+        }
+
+        public TEntity GetById(int id)
+        {
+            var result = this.Context.Set<TEntity>().Find(id);
+            return result;
+        }
+    }
+}
