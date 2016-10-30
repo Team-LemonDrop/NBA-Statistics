006cba7a5e34296674c751c16304d10c29237ba8
sguzunov <sguzunov@gmail.com>
2016-10-31 00:28:40 +0200

    Added PlayersRepository, StandingByDayRepository, TeamsRepository.

---
+++ NBAStatistics.Repositories/TeamsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+using NBAStatistics.Repositories.Contracts;
+
+namespace NBAStatistics.Repositories
+{
+    public class TeamsRepository<Team> : GenericRepository<Team>, IRepository<Team>
+        where Team : class
+    {
+        public TeamsRepository(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
