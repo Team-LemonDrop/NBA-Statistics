427bf7bfa56c0375c2b223de4932cb490cebcf1c
madmarks <bratmiro@gmail.com>
2016-10-31 05:34:51 +0200

    generate ZIP file with Excel reports  - get JSON from server  - fill Excel file with data  - pack all files and directories in ZIP file

---
+++ NBA-Stats/DailyStandings.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Stats
+{
+    public class DailyStandings
+    {
+        [JsonProperty("resource")]
+        public string Resource { get; set; }
+
+        [JsonProperty("parameters")]
+        public Parameters Parameters { get; set; }
+
+        [JsonProperty("resultsets")]
+        public Resultset[] ResultSets { get; set; }
+    }
+
+    public class Parameters
+    {
+        [JsonProperty("gamedate")]
+        public string GameDate { get; set; }
+
+        [JsonProperty("leagueid")]
+        public string LeagueId { get; set; }
+
+        [JsonProperty("dayoffset")]
+        public string DayOffset { get; set; }
+    }
+
+    public class Resultset
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("headers")]
+        public string[] Headers { get; set; }
+
+        [JsonProperty("rowset")]
+        public object[][] rowSet { get; set; }
+    }
+}
