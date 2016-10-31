a644f017e4e2b96c228cf2eadfba81b07e8878a6
madmarks <bratmiro@gmail.com>
2016-10-31 05:26:06 +0200

    add ConnectionProviders

---
+++ NBA-Stats/ConnectionProviders/MySqlConnectionProvider.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using NBA_Stats.ConnectionProviders.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Stats.ConnectionProviders
+{
+    public class MySqlConnectionProvider : IConnectionProvider
+    {
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            var connection = new MySqlConnection(connectionString);
+
+            return connection;
+        }
+    }
+}
