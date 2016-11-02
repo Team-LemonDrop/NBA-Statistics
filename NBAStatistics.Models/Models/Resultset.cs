3c83af26e2eca820315c58a32bebc19dfcaff8e9
madmarks <bratmiro@gmail.com>
2016-11-02 06:11:34 +0200

    NBA Daily Standings models

---
+++ NBAStatistics.Models/Models/Resultset.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NBAStatistics.Models.Models
+{
+    public class Resultset
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("headers")]
+        public string[] Headers { get; set; }
+
+        [JsonProperty("rowset")]
+        public object[][] RowSet { get; set; }
+    }
+}
