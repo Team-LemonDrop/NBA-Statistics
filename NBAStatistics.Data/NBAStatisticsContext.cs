2f9f73ba8c4c15fd455f61963852c1b5a557c488
sguzunov <sguzunov@gmail.com>
2016-11-02 17:35:55 +0200

    DbContext fully supports SQL Server models.

---
+++ NBAStatistics.Data/NBAStatisticsContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+
+using NBAStatistics.Data.Migrations;
+using NBAStatistics.Models;
+
+namespace NBAStatistics.Data
+{
+    public class NBAStatisticsContext : DbContext
+    {
+        public NBAStatisticsContext()
+            : base("name=NBAStatisticsConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<NBAStatisticsContext, Configuration>());
+        }
+
+        public IDbSet<Arena> Arenas { get; set; }
+
+        public IDbSet<City> Cities { get; set; }
+
+        public IDbSet<Conference> Conferences { get; set; }
+
+        public IDbSet<Country> Countries { get; set; }
+
+        public IDbSet<HeadCoach> HeadCoaches { get; set; }
+
+        public IDbSet<Player> Players { get; set; }
+
+        public IDbSet<School> Schools { get; set; }
+
+        public IDbSet<StandingsByDay> StandingsByDays { get; set; }
+
+        public IDbSet<Team> Teams { get; set; }
+    }
+}
