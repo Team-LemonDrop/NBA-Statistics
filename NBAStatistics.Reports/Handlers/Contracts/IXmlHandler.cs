d23efcffadebb59112c7e34c2814c13aa448c74c
sguzunov <sguzunov@gmail.com>
2016-11-05 17:23:22 +0200

    XML generator for played games report.

---
+++ NBAStatistics.Reports/Handlers/Contracts/IXmlHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace NBAStatistics.Reports.Handlers.Contracts
+{
+    public interface IXmlHandler<T> where T : class
+    {
+        void Serialize(IEnumerable<T> values, string rootPath);
+    }
+}
