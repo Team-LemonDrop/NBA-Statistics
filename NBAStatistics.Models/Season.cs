6c3bb1d5f51ac977a10d989a1804694061e8f1cd
sguzunov <sguzunov@gmail.com>
2016-11-05 10:04:42 +0200

    Keep information for player's points per game in a concrete season.

---
+++ NBAStatistics.Models/Season.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NBAStatistics.Models
+{
+    public class Season
+    {
+        private ICollection<PlayersSeasons> players;
+
+        public Season()
+        {
+            this.players = new HashSet<PlayersSeasons>();
+        }
+
+        public int Id { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public virtual ICollection<PlayersSeasons> Players
+        {
+            get { return this.players; }
+            set { this.players = value; }
+        }
+    }
+}
