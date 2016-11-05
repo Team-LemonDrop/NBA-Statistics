2fd931460f9c48e7a7230306b12e93cfe198b021
sguzunov <sguzunov@gmail.com>
2016-11-05 17:21:58 +0200

    Added service for working with JSON reports.

---
+++ NBAStatistics.Reports/Contracts/IJsonReportService.cs
@@ -0,0 +1,10 @@
+﻿using NBAStatistics.Data.Repositories.Contracts;
+using NBAStatistics.Models;
+
+namespace NBAStatistics.Reports.Contracts
+{
+    public interface IJsonReportService
+    {
+        void CreatePointsPerGameReport(IRepository<PlayersSeasons> playedSeasonsDataSource, int seasonStartYear, int seasonEndYear);
+    }
+}
