6f586b4f901ab70e5c48b3b61dbfc935dee56465
sguzunov <sguzunov@gmail.com>
2016-11-05 17:20:07 +0200

    JSON and XML reports pocos.

---
+++ NBAStatistics.Reports/Pocos/TeamDailyStandingsPoco.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NBAStatistics.Reports.Pocos
+{
+    public class TeamDailyStandingsPoco
+    {
+        public string TeamName { get; set; }
+
+        public IEnumerable<GamePoco> Games { get; set; }
+    }
+}
