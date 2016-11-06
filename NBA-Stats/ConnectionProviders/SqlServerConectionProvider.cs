be26569c8a255ddcffe144d13db2200408892a21
sguzunov <sguzunov@gmail.com>
2016-11-06 21:16:47 +0200

    Added abstract connection provider implementing basic connection string validation.

---
+++ NBA-Stats/ConnectionProviders/SqlServerConectionProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace NBA_Stats.ConnectionProviders
+{
+    public class SqlServerConectionProvider : ConnectionProvider
+    {
+        protected override IDbConnection GetConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
