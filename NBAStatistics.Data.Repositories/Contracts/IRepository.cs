b51828c99780b466b69c6d1938cad032472939d8
sguzunov <sguzunov@gmail.com>
2016-11-07 18:35:09 +0200

    Moved logic for repository to Data.Repositories assembly.

---
+++ NBAStatistics.Data.Repositories/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NBAStatistics.Data.Repositories.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        void Add(TEntity value);
+
+        void Update(TEntity value);
+
+        void Delete(TEntity value);
+    }
+}
---
+++ NBAStatistics.Data.Repositories/EfRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+using NBAStatistics.Data.Repositories.Contracts;
+
+namespace NBAStatistics.Data.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext dbContext;
+        private readonly IDbSet<T> dbSet;
+
+        public EfRepository(DbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext), "Database context cannot be null!");
+            }
+
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>();
+        }
+
+
+        public IEnumerable<T> GetAll()
+        {
+            return this.dbSet;
+        }
+
+        public void Add(T value)
+        {
+            var entry = AttachIfDetached(value);
+            entry.State = EntityState.Added;
+        }
+
+        public void Update(T entity)
+        {
+            var entry = AttachIfDetached(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            var entry = AttachIfDetached(entity);
+            entry.State = EntityState.Deleted;
+        }
+
+        private DbEntityEntry AttachIfDetached(T entity)
+        {
+            var entry = this.dbContext.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.dbSet.Attach(entity);
+            }
+
+            return entry;
+        }
+    }
+}
