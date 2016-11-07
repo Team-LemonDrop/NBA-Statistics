3f000f326fcbfc01c15292b11f0fdd320da086b8
madmarks <bratmiro@gmail.com>
2016-11-07 21:54:53 +0200

    introduce PlayerInfo class

---
+++ NBAStatistics.Models/Models.Json/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NBAStatistics.Models.Models.Json
+{
+    public class PlayerInfo
+    {
+        [JsonProperty("resource")]
+        public string Resource { get; set; }
+
+        [JsonProperty("parameters")]
+        public PlayerInfoParameter[] Parameters { get; set; }
+
+        [JsonProperty("resultsets")]
+        public Resultset[] ResultSets { get; set; }
+    }
+}
---
+++ NBAStatistics.Models/Models.Json/PlayerInfoParameter.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace NBAStatistics.Models.Models.Json
+{
+    public class PlayerInfoParameter
+    {
+        [JsonProperty("playerid")]
+        public int PlayerID { get; set; }
+
+        [JsonProperty("leagueid")]
+        public object LeagueID { get; set; }
+    }
+}
